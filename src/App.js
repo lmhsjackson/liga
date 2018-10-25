78c2b28c1e537a24ddd49ea11730742b7f816c77
Jose Piccioni <josepiccioni@gmail.com>
2018-10-25 11:39:37 -0300

    Add hotmodule and changed index

--- src/App.js
+++ src/App.js
@@ -4,22 +4,12 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var HelloWorld = 'Welcome to the Road to learn React!';
+    var User = {name: 'Jose', age: 32};
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h2>{HelloWorld}</h2>
+      <h1>{User.name} {User.age}</h1>
       </div>
     );
   }
