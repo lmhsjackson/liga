c0af6b3a232d0ce22b973a52ef7b8ad9b66173e4
Jose Piccioni <josepiccioni@gmail.com>
2018-11-14 17:00:07 -0300

    Add form with linked events.
      - Com. between components.

--- src/App.js
+++ src/App.js
@@ -21,14 +21,19 @@ const list = [
   },
 ];
 
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       list,
+      searchTerm: '',
     };
-    //this.onDismiss = this.onDismiss.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss = (id) => {
@@ -37,10 +42,21 @@ class App extends Component {
     this.setState({list: updatedList})
   }
 
+  onSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value })
+  }
+
   render() {
     return (
       <div className="App">
-      {this.state.list.map(item => 
+        <form>
+          <input 
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
+
+      {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
         <div key={item.objectID}>
           <span>
           <a href={item.url}>{item.title}</a> 
